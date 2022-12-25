65933888f3cc3db142daf7d588b062d3c0d59dfe
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-25 19:49:13 -0200

    Update mapping.sol

--- mapping.sol
+++ mapping.sol
@@ -2,9 +2,15 @@ pragma solidity 0.8.7;
 
 
 contract mappings{
-    mapping(address => uint )=> saldos;
+    mapping(address => uint ) saldos;
 
+    //depositar
     function deposit() external payable{
-        saldos[msg.sender] = msg.value;
+        saldos[msg.sender] += msg.value;
+    }
+
+    //consultar saldo
+    function balanceOf() external view returns(uint256){
+        return saldos[msg.sender];
     }
 }
