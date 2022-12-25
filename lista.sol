0e9c6168048236515c779680434399cdf669a2f7
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-25 18:37:27 -0200

    Update lista.sol

--- lista.sol
+++ lista.sol
@@ -2,5 +2,13 @@ pragma solidity 0.8.7;
 
 
 contract listas{
-    
+    string[] nome;
+
+    function salvarNome (string memory _nome ) external{
+        nome.push(_nome);
+    }
+
+    function visualizarNome() external view returns(string memory){
+        return nome[];
+    }
 }
