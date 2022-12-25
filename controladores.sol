1bb6902d760608defea7d298253c0370ce3ef34d
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-25 19:22:36 -0200

    Create controladores.sol

---
+++ controladores.sol
@@ -0,0 +1,11 @@
+pragma solidity 0.8.7;
+
+contract controladores{
+    address[] addresses;
+
+    function endereco(address _address) external {
+        addresses.push(_address);
+    }
+
+    function 
+}
