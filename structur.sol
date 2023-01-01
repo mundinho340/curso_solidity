8c9823c1b91a9cbcc91bf782be78966a77a368f2
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2022-12-31 23:53:39 -0200

    Update structur.sol

--- structur.sol
+++ structur.sol
@@ -6,6 +6,12 @@ contract marketPlace{
         uint id;
         string nome;
     }
+    Usuarios [] public usuarios;
+    function addUsuario(uint _id, string memory _nome) external{
+        Usuarios memory usuario1= Usuarios(msg.sender, _id, _nome);
+        usuarios.push(usuario1);
 
-    
+    }
+
+
 }
