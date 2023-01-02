b658094a00bbfbb50fcbcc0a81478de74c2d31ad
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-02 19:51:23 -0200

    Update Eventos.sol

--- Eventos.sol
+++ Eventos.sol
@@ -1,7 +1,7 @@
 pragma solidity 0.8.7;
 
 contract teste{
-    event Transferencia(address origem, address destino, uint256 quantidade);
+    event Transferencia(address indexed origem, address indexed destino, uint256 quantidade);
 
     function tranfer(address _destino, uint256 _quantidade) public {
 
