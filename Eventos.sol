ff36d0fdf41c8c1d4f5aac013903c0a04fee7106
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-02 19:38:07 -0200

    alterar_bug_na_linha_9

--- Eventos.sol
+++ Eventos.sol
@@ -3,9 +3,9 @@ pragma solidity 0.8.7;
 contract Eventos{
     event Transferencia(address _deque, address _paraonde, uint256 _quantidade);
 
-    function trasfer(address _paraonde, uint256 _quantidade) public {
+    function trasfer(address indexed _praque, uint256 indexed _quantidade) public {
         //codigos das verificacoes
 
-        emit Transferencia(msg.sender, _paraque, _quantidade);
+        emit Transferencia(msg.sender, _praque, _quantidade);
     }
 }
