d4173b3d520a8db45c7c3dbe1b0bc4de38d3ec67
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-04 19:34:47 -0200

    pagamentos

--- payme.sol
+++ payme.sol
@@ -1,5 +1,8 @@
 pragma solidity 0.8.7;
 
 contract pagaveis{
-    
+    function pagamento() external returns(bool){
+        require(msg.value>= 1 ether,"valor nao enviado por ser inferior a 1 ether");
+        return true;
+    }
 }
