e122640985c281dff99bf496e7142086c85ecb46
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-08 18:56:54 -0200

    Update typecast.sol

--- typecast.sol
+++ typecast.sol
@@ -4,6 +4,18 @@ contract typeCast{
     function change(address _address) external pure returns(uint160){
         return uint160(_address);
     }
+
+      function convertToByte20(address _address) external pure returns(bytes20){
+        return bytes20(_address);
+    }
+
+    function convertToByte4(address _address) external pure returns(bytes4){
+        return bytes4(bytes20(_address));
+    }
+
+    function convertToByte32(address _address) external pure returns(bytes32){
+        return bytes32(bytes20(_address));
+    }
 }
 
 
