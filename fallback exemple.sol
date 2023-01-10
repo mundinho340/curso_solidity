1b78dfc1bb0bf9931fd8019afbc6978663256914
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-01-10 11:59:09 -0200

    Update fallback exemple.sol

--- fallback exemple.sol
+++ fallback exemple.sol
@@ -5,6 +5,12 @@ contract fallbackexemple{
     uint public result;
 
     receive() external payable{
-        result += 1;
+        result = 1;
     }
+
+    fallback() external payable{
+        result =2;
+    }
+
+    // used 0x00 on trasection for call the fallback;
 }
