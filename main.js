811f87616c75affee8223a5e3abba001db1b0700
Raimundo Chigarisso <72931424+mundinho340@users.noreply.github.com>
2023-02-24 19:34:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,31 @@
-const ethers = require('ethers')
-const fs = require('fs-extra')
+const ethers = require("ethers");
+const fs = require("fs-extra");
 
-async function main(){
-  let provider = new ethers.providers.JsonRpcProvider( "HTTP://172.22.144.1:7545")
-
-  let wallet = new ethers.Wallet(
-    "6acada07cba5be8d777039c4836c674cd28d75663bc564286ba7135df68296ae",provider
+async function main() {
+  let provider = new ethers.providers.JsonRpcProvider(
+    "HTTP://172.22.144.1:7545"
   );
 
-  const abi =fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi",'utf8')
+  let wallet = new ethers.Wallet(
+    "0x67d2a2985a73dbf98acfb4556e5a5ecfc9e1cd42af164bb517b02e943ca04c63",
+    provider
+  );
+
+  const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
 
   const binary = fs.readFileSync(
     "./SimpleStorage_sol_SimpleStorage.bin",
     "utf8"
   );
 
-  const contractFactory = new ethers.ContractFactory(abi, binary,wallet)
-  console.log('Deploing, please wait ...')
-  const contract =  contractFactory.deploy()
-  console.log(contract)
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploing, please wait ...");
+  const contract = await contractFactory.deploy();
+  console.log(contract);
 }
 
 main()
-.then(()=> process.exi(0))
-.catch((error)=>{
-  process.exit(1)
-})
+  .then(() => process.exi(0))
+  .catch((error) => {
+    process.exit(1);
+  });
